ec26126df3f8d98bf7ccdf30bfd3c2cd2e660a89
Gappi <ganapatihegde588@gmail.com>
2022-09-17 14:12:26 +0530

    New Beginnings

---
+++ src/DSA/Arrays/ArrayBasics.java
@@ -0,0 +1,15 @@
+package DSA.Arrays;
+
+public class ArrayBasics {
+
+        public static void main(String[] args) {
+            int[] myArray = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                myArray[i] = i;
+                System.out.println(myArray[i]);
+            }
+        }
+
+
+}
